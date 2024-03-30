e7c4a2455c1915aa1178b30ad9c65ba5066118ac
Paulo Willian <paulowos@gmail.com>
2024-03-29 23:51:49 -0300

    Import React in Home.tsx and rearrange imports

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import React from "react";
 import Header from "../components/home/Header";
 
 export default function Home() {
