ee0a746ad424890ccea918d47469cb0780fe771a
Paulo Willian <paulowos@gmail.com>
2024-03-30 14:22:15 -0300

    Add SearchBar component to Header

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import HeaderTabs from "./HeaderTabs";
+import SearchBar from "./SearchBar";
 
 export default function Header() {
   return (
     <SafeAreaView style={{ backgroundColor: "white", padding: 15 }}>
       <HeaderTabs />
+      <SearchBar />
     </SafeAreaView>
   );
 }
