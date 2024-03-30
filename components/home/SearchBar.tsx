796a276898cd3c6c63d420166ca7a5f210d6368b
Paulo Willian <paulowos@gmail.com>
2024-03-30 14:22:10 -0300

    Add SearchBar component

---
+++ components/home/SearchBar.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { View } from "react-native";
+import {
+  GooglePlacesAutocomplete,
+  Query,
+} from "react-native-google-places-autocomplete";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+export default function SearchBar() {
+  const query: Query = {
+    key: process.env.EXPO_PUBLIC_PLACES_API_KEY || "",
+    language: "pt-BR",
+  };
+
+  return (
+    <View
+      style={{
+        marginTop: 15,
+        flexDirection: "row",
+      }}
+    >
+      <GooglePlacesAutocomplete
+        placeholder="Pesquisar"
+        query={query}
+        styles={{
+          textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 30,
+            fontWeight: "700",
+          },
+          textInputContainer: {
+            backgroundColor: "#eee",
+            borderRadius: 30,
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
+          },
+        }}
+        renderLeftButton={() => (
+          <Ionicons
+            name="location-sharp"
+            size={24}
+            style={{ marginLeft: 10 }}
+          />
+        )}
+        renderRightButton={() => (
+          <Ionicons name="search-sharp" size={24} style={{ marginRight: 10 }} />
+        )}
+      />
+    </View>
+  );
+}
