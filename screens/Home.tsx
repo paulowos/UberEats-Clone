f82838f92d527d7a04369d2c3bc55d554597296b
Paulo Willian <paulowos@gmail.com>
2024-03-29 22:42:33 -0300

    Add Home screen component

---
+++ screens/Home.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import React from "react";
+import Header from "../components/home/Header";
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <Header />
+    </SafeAreaView>
+  );
+}
