ecb30e1791953e7cb0a295ecf801a261b3395276
Paulo Willian <paulowos@gmail.com>
2024-03-29 23:51:36 -0300

    Refactor Header component to use HeaderTabs

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View } from "react-native";
+
+import HeaderTabs from "./HeaderTabs";
 
 export default function Header() {
   return (
     <View>
-      <Text>Header</Text>
+      <HeaderTabs />
     </View>
-  )
+  );
 }
