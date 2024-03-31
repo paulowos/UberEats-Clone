666da794958be35ebc4caff55b0fa417f9d70283
Paulo Willian <paulowos@gmail.com>
2024-03-31 18:39:20 -0300

    Add Body component to Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 
+import Body from "../components/home/body/Body";
 import Header from "../components/home/header/Header";
 
 export default function Home() {
   return (
     <View style={{ backgroundColor: "#eee", flex: 1 }}>
       <Header />
+      <Body />
     </View>
   );
 }
