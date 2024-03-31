7a9306d07c98e36f9725f0717aa75ef262e9b188
Paulo Willian <paulowos@gmail.com>
2024-03-30 22:26:07 -0300

    Add Categories component to Header

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import Categories from "./Categories";
 import HeaderTabs from "./HeaderTabs";
 import SearchBar from "./SearchBar";
 
 export default function Header() {
   return (
-    <SafeAreaView style={{ backgroundColor: "white", padding: 15 }}>
-      <HeaderTabs />
-      <SearchBar />
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={{ backgroundColor: "white", paddingVertical: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+        <Categories />
+      </SafeAreaView>
+    </>
   );
 }
