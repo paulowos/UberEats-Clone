4e9e44ba6ad7112159333f0b269a84dc9be0c73c
Paulo Willian <paulowos@gmail.com>
2024-03-30 22:26:03 -0300

    Refactor Categories component to display scrollable list of categories

--- src/components/home/Categories.tsx
+++ src/components/home/Categories.tsx
@@ -1,10 +1,44 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
+import categoriesAssets, {
+  categoryAsset,
+} from "../../assets/images/categories";
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
-    </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      <View style={styles.categoriesScrollView}>
+        {categoriesAssets.map((asset) => (
+          <Category name={asset.name} image={asset.image} key={asset.name} />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
+
+const Category = ({ name, image }: categoryAsset) => (
+  <TouchableOpacity style={styles.categoryView}>
+    <Image source={image} style={styles.categoryImage} />
+    <Text style={styles.categoryText}>{name}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  categoriesScrollView: {
+    flexDirection: "row",
+    gap: 30,
+    paddingHorizontal: 15,
+    marginTop: 15,
+  },
+  categoryImage: { width: 50, height: 40, resizeMode: "contain" },
+  categoryText: { fontSize: 13, fontWeight: "900" },
+  categoryView: { alignItems: "center" },
+});
