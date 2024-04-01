6045a96c0d6f021de33ba0828c11a16ada962646
Paulo Willian <paulowos@gmail.com>
2024-04-01 11:45:50 -0300

    Add aliases to category assets

--- src/assets/images/categories.ts
+++ src/assets/images/categories.ts
@@ -3,15 +3,20 @@ import { ImageURISource } from "react-native";
 export type categoryAsset = {
   name: string;
   image: ImageURISource;
+  alias: string;
 };
 
 const categoriesAssets: categoryAsset[] = [
-  { name: "Fast Foods", image: require("./fast-food.png") },
-  { name: "Sobremesas", image: require("./desserts.png") },
-  { name: "Bebidas", image: require("./soft-drink.png") },
-  { name: "Padarias", image: require("./bread.png") },
-  { name: "Cafeterias", image: require("./coffee.png") },
-  { name: "Mercados", image: require("./deals.png") },
+  { name: "Fast Foods", image: require("./fast-food.png"), alias: "hotdogs" },
+  { name: "Sobremesas", image: require("./desserts.png"), alias: "desserts" },
+  {
+    name: "Bebidas",
+    image: require("./soft-drink.png"),
+    alias: "beverage_stores",
+  },
+  { name: "Padarias", image: require("./bread.png"), alias: "bakeries" },
+  { name: "Cafeterias", image: require("./coffee.png"), alias: "cafes" },
+  { name: "Mercados", image: require("./deals.png"), alias: "grocery" },
 ];
 
 export default categoriesAssets;
