f973417e15647c4307fe2d3e0245816e341b57d8
Paulo Willian <paulowos@gmail.com>
2024-03-31 22:50:38 -0300

    Add location state to Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 
 import Body from "../components/home/body/Body";
 import Header from "../components/home/header/Header";
 
 export default function Home() {
+  const [location, setLocation] = useState("Rio%20de%20Janeiro");
+
   return (
     <View style={{ backgroundColor: "#eee", flex: 1 }}>
-      <Header />
-      <Body />
+      <Header setLocation={setLocation} />
+      <Body location={location} />
     </View>
   );
 }
