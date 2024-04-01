009e1aba42594c8298ecaa86c41fea34afa060e8
Paulo Willian <paulowos@gmail.com>
2024-04-01 11:46:11 -0300

    Refactor Category component to handle alias selection

--- src/components/home/header/Categories.tsx
+++ src/components/home/header/Categories.tsx
@@ -12,24 +12,40 @@ import categoriesAssets, {
   categoryAsset,
 } from "../../../assets/images/categories";
 
+import useStore, { DEFAULT_ALIAS } from "../../../store/store";
+
 export default function Categories() {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       <View style={styles.categoriesScrollView}>
         {categoriesAssets.map((asset) => (
-          <Category name={asset.name} image={asset.image} key={asset.name} />
+          <Category
+            name={asset.name}
+            image={asset.image}
+            alias={asset.alias}
+            key={asset.name}
+          />
         ))}
       </View>
     </ScrollView>
   );
 }
 
-const Category = ({ name, image }: categoryAsset) => (
-  <TouchableOpacity style={styles.categoryView}>
-    <Image source={image} style={styles.categoryImage} />
-    <Text style={styles.categoryText}>{name}</Text>
-  </TouchableOpacity>
-);
+const Category = ({ name, image, alias }: categoryAsset) => {
+  const store = useStore();
+
+  const handlePress = () => {
+    if (store.alias == alias) return store.setAlias(DEFAULT_ALIAS);
+    store.setAlias(alias);
+  };
+
+  return (
+    <TouchableOpacity style={styles.categoryView} onPress={handlePress}>
+      <Image source={image} style={styles.categoryImage} />
+      <Text style={styles.categoryText}>{name}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   categoriesScrollView: {
