fe71a97281a68b9f70774a13bd761dd8a31bccb5
Paulo Willian <paulowos@gmail.com>
2024-04-01 20:50:49 -0300

    Add footer component to Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { View } from "react-native";
+import { Divider } from "react-native-elements";
 
 import Body from "../components/home/body";
+import Footer from "../components/home/footer";
 import Header from "../components/home/header";
 
 export default function Home() {
@@ -9,6 +11,8 @@ export default function Home() {
     <View style={{ backgroundColor: "#eee", flex: 1 }}>
       <Header />
       <Body />
+      <Divider width={1} />
+      <Footer />
     </View>
   );
 }
