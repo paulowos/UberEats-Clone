9d7534bbed094122c39cff2f342681201d55d8c9
Paulo Willian <paulowos@gmail.com>
2024-04-02 10:14:15 -0300

    Add Detail screen component

---
+++ src/screens/Detail.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import About from "../components/detail/About";
+
+export default class Detail extends Component {
+  render() {
+    return (
+      <View>
+        <About />
+      </View>
+    );
+  }
+}
