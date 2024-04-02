8f610c3825fba15b1307fdd5701efb7edf6a3d7b
Paulo Willian <paulowos@gmail.com>
2024-04-02 10:48:17 -0300

    Import order changes in Navigation.tsx

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,9 +1,9 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "../screens/Home";
 import Detail from "../screens/Detail";
+import Home from "../screens/Home";
 import StackParamType from "../types/stackParamsType";
 
 export default function Navigation() {
