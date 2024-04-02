4cf14e6ce3abd37654b0fe3d19e7eb45881856bc
Paulo Willian <paulowos@gmail.com>
2024-04-02 10:44:22 -0300

    Update navigation to use createNativeStackNavigator

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "../screens/Home";
 import Detail from "../screens/Detail";
+import StackParamType from "../types/stackParamsType";
 
 export default function Navigation() {
-  const Stack = createStackNavigator();
+  const Stack = createNativeStackNavigator<StackParamType>();
+
   const screenOptions = { headerShown: false };
+
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Navigator screenOptions={screenOptions} initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Detail" component={Detail} />
       </Stack.Navigator>
