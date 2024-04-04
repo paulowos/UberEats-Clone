057f5116c56ce3edb7101bbcd6fb7790bc969dff
Paulo Willian <paulowos@gmail.com>
2024-04-04 11:39:28 -0300

    Add Divider and Menu components to Detail screen

--- src/screens/Detail.tsx
+++ src/screens/Detail.tsx
@@ -1,16 +1,25 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { View } from "react-native";
+import { Divider } from "react-native-elements";
 
+import getMenuItems from "../assets/menuItems";
 import About from "../components/detail/About";
+import Menu from "../components/detail/Menu";
 import StackParamType from "../types/stackParamsType";
 
 type Props = NativeStackScreenProps<StackParamType, "Detail">;
 
 export default function Detail({ route }: Props) {
+  const seed = route.params.rating + route.params.review_count;
+
+  const foods = getMenuItems(seed);
+
   return (
-    <View>
+    <View style={{ height: "100%" }}>
       <About route={route} />
+      <Divider width={1.8} style={{ marginTop: 10 }} />
+      <Menu foods={foods} />
     </View>
   );
 }
