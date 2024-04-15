cfbd76e12b032e5021762ae7e67be23267dae378
Paulo Willian <paulowos@gmail.com>
2024-04-14 23:24:11 -0300

    Add cart functionality to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,15 @@
 import { create } from "zustand";
 
+import FoodType from "../types/foodType";
+
 type Store = {
   location: string;
   setLocation: (location: string) => void;
   alias: string;
   setAlias: (alias: string) => void;
+  cart: FoodType[];
+  setCart: (food: FoodType) => void;
+  total: number;
 };
 
 export const DEFAULT_ALIAS =
@@ -15,6 +20,25 @@ const useStore = create<Store>((set) => ({
   setLocation: (location) => set({ location }),
   alias: DEFAULT_ALIAS,
   setAlias: (alias) => set({ alias }),
+  cart: [],
+  setCart: (food) =>
+    set((state) => {
+      if (state.cart.some((f) => f.id === food.id)) {
+        const newCart = state.cart.filter((f) => f.id !== food.id);
+        const total = newCart.reduce((acc, f) => {
+          const price = parseFloat(f.price.replace("R$", "").replace(",", "."));
+          return acc + price;
+        }, 0);
+        return { cart: newCart, total };
+      }
+      return {
+        cart: [...state.cart, food],
+        total:
+          state.total +
+          parseFloat(food.price.replace("R$", "").replace(",", ".")),
+      };
+    }),
+  total: 0,
 }));
 
 export default useStore;
